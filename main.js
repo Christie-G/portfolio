e9f0d3511690fcaa40339d5d7927efd093167f9f
Christie-G <christiegriffith@gmail.com>
2016-10-05 10:19:38 -0700

    progress on about uploaded port images

--- main.js
+++ main.js
@@ -2,6 +2,20 @@
 
 
 $( document ).ready(function() {
+
+$(".floating-menu").hide(); //Hide the navigation bar first
+
+    $(window).scroll(function () {  //Listen for the window's scroll event
+        if (isScrolledAfterElement('#top')) { //if it has scrolled beyond the #content elment
+            $('.floating-menu').show();  //Show the navigation bar
+        } else {
+            $('.floating-menu').hide(); //Else hide it
+        }
+
+    });
+
+
+
 var myList = [ "Websites", "Buisness Solutions", "Graphics", "Marketing Plans", "Events", "Lean Start-Ups","Corporate Culture", "Training Materials", "For YOU!" ];
 
 var delay=1000;
@@ -15,4 +29,6 @@ $('#textholder').html(listValue);
 delay+=1000
 
 });
+
+
 });
