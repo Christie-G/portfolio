de5f9a88590e8ee3db5fbf2437a3bed1d82dedfa
Christie-G <christiegriffith@gmail.com>
2016-10-05 02:17:42 -0700

    finished cover setup section

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+
+
+
 $( document ).ready(function() {
-        console.log( "document loaded" );
-    });
+var myList = [ "Websites", "Buisness Solutions", "Graphics", "Marketing Plans", "Events", "Lean Start-Ups","Corporate Culture", "Training Materials", "For YOU!" ];
+
+var delay=1000;
+
+$.each(myList,function(listNo,listValue){
+setTimeout (function(){
+
+$('#textholder').html(listValue);
+
+}, delay );
+delay+=1000
+
+});
+});
